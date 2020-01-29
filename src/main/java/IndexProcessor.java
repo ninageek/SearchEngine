4b042d6f83e3239e8ded4c5e11dc768591f0bcd1
ninageek <icefrog911@yahoo.com>
2020-01-29 17:34:15 -0600

    optimize imports

--- src/main/java/IndexProcessor.java
+++ src/main/java/IndexProcessor.java
@@ -2,7 +2,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
