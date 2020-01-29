00389e8ed30a048ce12c4007da3c35d3a10e3d70
ninageek <icefrog911@yahoo.com>
2020-01-29 17:12:21 -0600

    add nullCheck to IndexProcessor

--- src/main/java/IndexProcessor.java
+++ src/main/java/IndexProcessor.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
@@ -11,15 +13,19 @@ public class IndexProcessor {
 
         List<String> pageContent = new ArrayList<>();
         ClassLoader classLoader = this.getClass().getClassLoader();
-        File file = new File(classLoader.getResource(myFile).getFile());
-        try {
-            Scanner pageReader = new Scanner(file);
-            while (pageReader.hasNextLine()) {
-                pageContent.add(pageReader.nextLine());
+        URL resource = classLoader.getResource(myFile);
+
+        if (resource != null) {
+            File file = new File(resource.getFile());
+            try {
+                Scanner pageReader = new Scanner(file);
+                while (pageReader.hasNextLine()) {
+                    pageContent.add(pageReader.nextLine());
+                }
+                pageReader.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
             }
-            pageReader.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         }
         return pageContent;
     }
